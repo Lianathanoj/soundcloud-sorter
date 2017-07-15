24302b8b28b43399563414d9c884b1b038a4ed85
Jonathan Lian <jlian9@gatech.edu>
2017-07-15 02:40:00 -0400

    fundamental api endpoints and requests work, but app still needs to be refactored

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import CategorySelector from './CategorySelector';
 import SearchBar from './SearchBar';
 import SongContainer from './SongContainer';
+var SC = require('soundcloud');
+var axios = require('axios');
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +14,27 @@ class App extends Component {
           numSongs: 0,
           sortType: "default",
       };
+      this.initializeSoundCloud();
+  }
+
+  initializeSoundCloud() {
+      SC.initialize({
+          client_id: 'kMY1mdglCWG4Y1hgWMc5P7T60bK5pKT9',
+          redirect_uri: ''
+      });
+  }
+
+  testLoadWidgets = () => {
+      axios.get("https://api-v2.soundcloud.com/search/queries?q=test&client_id=2t9loNQH90kzJcsFCODdigxfp325aq4z&limit=10&offset=0&linked_partitioning=1&app_version=1500036064").then(response => console.log(response));
+      SC.resolve("https://soundcloud.com/oshi/tracks").then(function(tracks) {
+          console.log(tracks);
+      });
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <script src="https://connect.soundcloud.com/sdk/sdk-3.1.2.js"></script>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>SoundCloud Specific Song Sorter</h2>
         </div>
@@ -27,6 +43,7 @@ class App extends Component {
           <CategorySelector />
           <SongContainer />
         </p>
+        <button onClick={this.testLoadWidgets}>testing</button>
       </div>
     );
   }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,14 +4,25 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: "Input an artist's profile URL."
+            text: ""
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    handleChange(e) {
+        this.setState({text: e.target.value});
     }
 
     render() {
         return (
-            <form>
-                <input value={this.state.text}></input>
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="Input an artist's profie URL." value={this.state.text} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
             </form>
         );
     }
